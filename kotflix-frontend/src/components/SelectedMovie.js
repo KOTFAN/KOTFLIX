adf23b7c16b7be43b36736aacef56744a14b6e4e
KOTFAN <kotfan.empire@gmail.com>
2025-08-12 19:59:39 +0300

    styled SelectedMovie component

--- kotflix-frontend/src/components/SelectedMovie.js
+++ kotflix-frontend/src/components/SelectedMovie.js
@@ -1,31 +1,61 @@
 import { useEffect, useState } from "react";
+import StarRating from "./StarRating/StarRating";
+import Preloader from "./Preloader";
 
 function SelectedMovie({ movieId, closeMovieInfoHandler }) {
   const [movieData, setMovieData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function getMovieDetailsHandler(id) {
     const APIKEY = process.env.REACT_APP_OMDB_API_KEY;
+    setIsLoading(true);
     fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${id}`)
       .then((data) => data.json())
       .then((movieInfo) => {
         setMovieData(movieInfo);
       })
-      .catch((err) => console.log("my err🔥", err));
+      .catch((err) => {
+        console.log("my err🔥", err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }
   useEffect(() => {
     getMovieDetailsHandler(movieId);
   }, [movieId]);
   return (
     <>
-      {movieData ? (
-        <div>
-          <span>{movieData.Title}</span>
-          <button className="btn-back" onClick={closeMovieInfoHandler}>
-            &larr;
-          </button>
-        </div>
+      {!movieData || isLoading ? ( //will fix in future)
+        <Preloader />
       ) : (
-        <div>Loading...</div>
+        <div className="details">
+          <header>
+            {console.log(movieData)}
+            <button className="btn-back" onClick={closeMovieInfoHandler}>
+              ⬅
+            </button>
+            <img src={movieData?.Poster} alt={"image of " + movieData.Title} />
+            <div className="details-overview">
+              <h3>{movieData.Title}</h3>
+              <p>
+                {movieData.Released} &bull; {movieData.Runtime}
+              </p>
+              <p> {movieData.Genre}</p>
+              <p>
+                <span>⭐{movieData.imdbRating} IMDB Rating</span>
+              </p>
+            </div>
+          </header>
+          <section>
+            <StarRating key={movieId} maxStars={10} size={25} />
+            <p>
+              <em>{movieData.Plot}</em>
+            </p>
+            <p>Actors {movieData.Actors}</p>
+            <p>Directed by {movieData.Director}</p>
+          </section>
+        </div>
       )}
     </>
   );
