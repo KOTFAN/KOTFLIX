e7fd3db573517d4b2e5e3bcc391d4680c579a425
KOTFAN <kotfan.empire@gmail.com>
2025-08-15 16:44:54 +0300

    add useMovies

--- kotflix-frontend/src/components/App.js
+++ kotflix-frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import NavBar from "./NavBar";
 import Main from "./Main";
@@ -11,20 +11,17 @@ import Summary from "./Summary";
 import Preloader from "./Preloader";
 import ErrorMessage from "./ErrorMessage";
 import SelectedMovie from "./SelectedMovie";
-
-const APIKEY = process.env.REACT_APP_OMDB_API_KEY;
+import { useMovies } from "../hooks/useMovies";
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
+  const { movies, isLoading, errorMessage, searchMoviesHandler } = useMovies();
   const [watched, setWatched] = useState(() => {
     const isStored = localStorage.getItem("watched");
     return isStored ? JSON.parse(localStorage.getItem("watched")) : [];
   });
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+
   const [selectedMovieId, setSelectedMovieId] = useState(null);
   const [ratedMovie, setRatedMovie] = useState(null);
-  const controller = useRef(null);
 
   function selectMovieHandler(id) {
     setSelectedMovieId((currentId) => (currentId === id ? null : id));
@@ -47,60 +44,6 @@ export default function App() {
 
   function deleteWatchedMovieHandler(movieId) {
     setWatched((c) => c.filter((item) => item.imdbID !== movieId));
-  }
-
-  function searchMoviesHandler(searchQuery) {
-    if (controller.current) {
-      controller.current.abort();
-    }
-
-    controller.current = new AbortController();
-    async function getMovies(searchQuery) {
-      try {
-        setIsLoading(true);
-        setErrorMessage("");
-
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchQuery}`,
-          { signal: controller.current.signal }
-        );
-
-        if (!res.ok) throw new Error("Failed to load movies data");
-
-        const data = await res.json();
-
-        if (data.Response === "False") {
-          throw new Error("Movies not found");
-        }
-
-        setMovies(
-          data.Search.map((movie) => {
-            return {
-              ...movie,
-              title: movie.Title,
-              runtime: movie.Runtime,
-              imdbRating: movie.imdbRating,
-              imdbID: movie.imdbID,
-              poster: movie.Poster,
-            };
-          })
-        );
-
-        setErrorMessage("");
-      } catch (error) {
-        if (error.name !== "AbortError") {
-          setErrorMessage(error.message);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    if (searchQuery.length < 3) {
-      setMovies([]);
-      return;
-    }
-    getMovies(searchQuery);
   }
 
   useEffect(() => {
---
+++ kotflix-frontend/src/hooks/useMovies.js
@@ -0,0 +1,65 @@
+import { useRef, useState } from "react";
+
+const APIKEY = process.env.REACT_APP_OMDB_API_KEY;
+export function useMovies() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const controller = useRef(null);
+
+  function searchMoviesHandler(searchQuery) {
+    if (controller.current) {
+      controller.current.abort();
+    }
+
+    controller.current = new AbortController();
+    async function getMovies(searchQuery) {
+      try {
+        setIsLoading(true);
+        setErrorMessage("");
+
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${APIKEY}&s=${searchQuery}`,
+          { signal: controller.current.signal }
+        );
+
+        if (!res.ok) throw new Error("Failed to load movies data");
+
+        const data = await res.json();
+
+        if (data.Response === "False") {
+          throw new Error("Movies not found");
+        }
+
+        setMovies(
+          data.Search.map((movie) => {
+            return {
+              ...movie,
+              title: movie.Title,
+              runtime: movie.Runtime,
+              imdbRating: movie.imdbRating,
+              imdbID: movie.imdbID,
+              poster: movie.Poster,
+            };
+          })
+        );
+
+        setErrorMessage("");
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          setErrorMessage(error.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    if (searchQuery.length < 3) {
+      setMovies([]);
+      return;
+    }
+    getMovies(searchQuery);
+  }
+
+  return { movies, isLoading, errorMessage, searchMoviesHandler };
+}
